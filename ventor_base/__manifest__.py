660f15827ef501d7128c52d728037856c84a9b50
kirill <kirill.v@ventor.tech>
2025-11-20 05:54:24 +0000

    updated version

--- ventor_base/__manifest__.py
+++ ventor_base/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Ventor Base',
-    'version': '15.0.2.3.0',
+    'version': '15.0.2.4.0',
     'author': 'VentorTech',
     'website': 'https://ventor.tech/',
     'license': 'LGPL-3',
