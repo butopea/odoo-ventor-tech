2454fd41ac9fc386fc5adb894059e622f5b180a6
kirill <kirill.v@ventor.tech>
2025-12-22 22:04:26 +0000

    [MERPWH-1829] added filtering of package types from carrier

--- ventor_base/wizard/multiple_pack_wizard.py
+++ ventor_base/wizard/multiple_pack_wizard.py
@@ -3,13 +3,19 @@
 
 import math
 
-from odoo import fields, models
+from odoo import api,fields, models
 from odoo.exceptions import UserError
 
 
 class MultiplePackWizard(models.TransientModel):
     _name = 'multiple.pack.wizard'
     _description = 'Wizard: Multiple Packages'
+
+    allowed_package_type_ids = fields.Many2many(
+        'stock.package.type',
+        compute='_compute_allowed_package_type_ids',
+        readonly=True,
+    )
 
     move_line_id = fields.Many2one(
         'stock.move.line',
@@ -37,6 +43,20 @@ class MultiplePackWizard(models.TransientModel):
         'stock.package.type',
         string="Package Type",
     )
+
+    @api.depends('move_line_id.move_id.picking_id.carrier_id')
+    def _compute_allowed_package_type_ids(self):
+        package_type_model = self.env['stock.package.type']
+        for wizard in self:
+            carrier = wizard.move_line_id.move_id.picking_id.carrier_id
+            if carrier:
+                allowed_ids = package_type_model.search([
+                    ('package_carrier_type', '=', carrier.delivery_type),
+                ]).ids
+            else:
+                allowed_ids = package_type_model.search([]).ids
+
+            wizard.allowed_package_type_ids = [(6, 0, allowed_ids)]
 
     def action_pack(self):
         self.ensure_one()
