4352e36400bdf1fa89184445a588ac9abf3eaf0f
kirill <kirill.v@ventor.tech>
2025-12-23 07:11:40 +0000

    [MERPWH-1829] fixed default_quantity_total

--- ventor_base/models/stock_move_line.py
+++ ventor_base/models/stock_move_line.py
@@ -18,6 +18,6 @@ class StockMoveLine(models.Model):
             'target': 'new',
             'context': {
                 'default_move_line_id': self.id,
-                'default_quantity_total': self.reserved_qty,
+                'default_quantity_total': self.product_uom_qty,
             }
         }
