5f7a14c89443f0ac5f67fe9f3a0a2f78577376b9
kirill <kirill.v@ventor.tech>
2026-02-24 14:31:38 +0000

    removed task MERPWH-1938

--- ventor_base/models/stock_picking.py
+++ ventor_base/models/stock_picking.py
@@ -1,6 +1,4 @@
 from odoo import fields, models, api, _
-from odoo.exceptions import UserError
-from odoo.tools.float_utils import float_compare
 
 
 class StockPickingType(models.Model):
@@ -178,11 +176,6 @@ class StockPickingType(models.Model):
     picking_ids = fields.One2many(
         comodel_name="stock.picking",
         inverse_name="picking_type_id",
-    )
-
-    prohibit_validation_incomplete_transfer = fields.Boolean(
-        string="Prohibit Validation for incomplete transfers",
-        help="Disables validation until all expected quantities are confirmed",
     )
 
     quality_check_per_product_line = fields.Boolean(
@@ -401,22 +394,3 @@ class StockPickingType(models.Model):
                 "quality_check_per_product_line": self.quality_check_per_product_line,
             }
         }
-
-
-class Picking(models.Model):
-    _inherit = "stock.picking"
-
-    def button_validate(self):
-        for picking in self:
-            if picking.picking_type_id.prohibit_validation_incomplete_transfer:
-                for move in picking.move_ids.filtered(lambda m: m.state not in ("done", "cancel")):
-                    if float_compare(
-                        move.quantity_done,
-                        move.product_uom_qty,
-                        precision_rounding=move.product_uom.rounding,
-                    ) < 0:
-                        raise UserError(_(
-                            "This transfer contains unprocessed products. Review the item list "
-                            "and ensure all quantities are processed before validating"
-                        ))
-        return super(Picking, self).button_validate()
